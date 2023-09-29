941988abd7fd79b82b68c776fe2a5616a3b54a43
Felipe <fcalldas@gmail.com>
2023-09-29 20:55:21 -0300

    ✨ create a simple part of the home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,69 +1,45 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+main() => runApp(const Leveler());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Leveler extends StatelessWidget {
+  const Leveler({super.key});
 
-  
   @override
   Widget build(BuildContext context) {
+    final questions = [
+      'Have you ever learned programming logic?',
+      'Have you ever learned any programming languages?',
+    ];
+
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Which level am i ?'),
+          ),
+          body: Center(
+            child: Column(
+              children: <Widget>[
+                Text(questions[0]),
+                const ElevatedButton(
+                  onPressed: null,
+                  child: Text('Anwser 1'),
+                ),
+                const ElevatedButton(
+                  onPressed: null,
+                  child: Text('Anwser 2'),
+                ),
+                const ElevatedButton(
+                  onPressed: null,
+                  child: Text('Anwser 3'),
+                ),
+                const ElevatedButton(
+                  onPressed: null,
+                  child: Text('Anwser 4'),
+                )
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), 
+          )),
     );
   }
 }
